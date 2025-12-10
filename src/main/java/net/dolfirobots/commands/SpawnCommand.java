c589f7f84eee9ddfab1ee018e68ab505c4b679f5
Dolfirobots <127861291+Dolfirobots@users.noreply.github.com>
2025-12-10 19:35:40 +0100

    feat: add listener

--- src/main/java/net/dolfirobots/commands/SpawnCommand.java
+++ src/main/java/net/dolfirobots/commands/SpawnCommand.java
@@ -38,6 +38,9 @@ public class SpawnCommand implements CommandExecutor, TabCompleter {
     }
 
     public static void register() {
+        Bukkit.getScheduler().runTaskTimer(Main.getInstance(), new SpawnCommand(), 5L, 100L);
+        Bukkit.getPluginManager().registerEvents(new SpawnCommand(), Main.getInstance());
+        
         Main.getInstance().getCommand("spawn").setExecutor(new SpawnCommand());
         Main.getInstance().getCommand("spawn").setTabCompleter(new SpawnCommand());
     }
